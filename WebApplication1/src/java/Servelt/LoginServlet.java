e3d7280310ab1a3d36b4591c7421f51c4984eda9
kecaron <caron_kevin95@yahoo.fr>
2016-05-10 23:02:08 +0200

    Look

---
+++ WebApplication1/src/java/Servelt/LoginServlet.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servelt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import sql.connexion;
+
+/**
+ *
+ * @author caron
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String name = request.getParameter("name");
+System.out.println("Login :"+name);
+String pass = request.getParameter("pass");
+System.out.println("Pass :"+pass);
+int count=-2;
+String url = "jdbc:oracle:thin:@localhost:1521:XE";
+String sql = "SELECT COUNT(mail_perso) AS ttc FROM PROFIL WHERE (mail_perso='"+name+"' OR mail_up10='"+name+"') AND password='"+pass+"'";
+System.out.println("Querry :"+sql);
+connexion cx = new connexion("system", "mamans90", url);
+cx.connectionbase();
+cx.create_statement();
+ResultSet p = cx.exec(sql);
+if (p == null) {
+    count =-1;
+} else {
+    while (p.next()) {
+
+     count=p.getInt("ttc");
+    }
+}
+// if count !=1 rejette 
+if (count!=1)
+{
+    RequestDispatcher rd = request.getRequestDispatcher("/error.jsp");
+    System.out.println("count : "+count);
+    
+    rd.forward(request, response);
+
+} else {
+    RequestDispatcher rd = request.getRequestDispatcher("/panel.jsp");
+    System.out.println("count : "+count);
+    rd.forward(request, response);
+}
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
